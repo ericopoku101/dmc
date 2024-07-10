49296a43e151f6bde4d3597dfba937c9287978fd
ericopoku101 <127147688+ericopoku101@users.noreply.github.com>
2024-07-10 07:32:31 +0000

    commit #1

---
+++ static/js/cart.js
@@ -0,0 +1,14 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+console.log('hello world')
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		
+
+		
+	})
+}
+
